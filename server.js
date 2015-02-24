349580c0ad530244e1570442ee51cf39bac4b047
Matt <Matt@Deep-Thought.local>
2015-02-23 23:06:59 -0700

    Add MongoDb to archietcture

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+// Includes
+
 var express = require('express'),
     stylus = require('stylus'),
     logger = require('morgan'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
+
+// Server config
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -24,13 +29,35 @@ app.use(stylus.middleware(
 ));
 app.use(express.static(__dirname + '/public'));
 
+// DB
+
+mongoose.connect('mongodb://localhost/meanapp');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+    console.log('meanapp db opened');
+});
+
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc) {
+    mongoMessage = messageDoc.message;
+});
+
+// Routes
+
 app.get('/partials/:partialPath', function(req, res) {
     res.render('partials/' + req.params.partialPath);
 });
 
 app.get('*', function(req, res) {
-    res.render('index');
+    res.render('index', {
+        mongoMessage: mongoMessage
+    });
 });
+
+// Server initialization
 
 var port = 3030;
 app.listen(port);
